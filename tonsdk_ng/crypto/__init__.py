3bb235bc6d8026c4502ad55fd7c85a458e92a8fd
andrey.torsunov <andrey.torsunov@gmail.com>
2024-04-18 17:25:29 +0300

    feat: add mnemonic generation from password

--- tonsdk_ng/crypto/__init__.py
+++ tonsdk_ng/crypto/__init__.py
@@ -1,14 +1,20 @@
 from ._keystore import generate_keystore_key, generate_new_keystore
-from ._mnemonic import mnemonic_is_valid, mnemonic_new, mnemonic_to_wallet_key
+from ._mnemonic import (
+    mnemonic_from_password,
+    mnemonic_is_valid,
+    mnemonic_new,
+    mnemonic_to_wallet_key,
+)
 from ._utils import private_key_to_public_key, verify_sign
 
 __all__ = [
+    "generate_key_pair",
+    "generate_keystore_key",
+    "generate_new_keystore",
+    "mnemonic_from_password",
+    "mnemonic_is_valid",
     "mnemonic_new",
     "mnemonic_to_wallet_key",
-    "mnemonic_is_valid",
-    "generate_new_keystore",
-    "generate_keystore_key",
     "private_key_to_public_key",
     "verify_sign",
-    "generate_key_pair",
 ]
--- tonsdk_ng/crypto/_mnemonic.py
+++ tonsdk_ng/crypto/_mnemonic.py
@@ -1,5 +1,6 @@
 import hashlib
 import hmac
+import random
 
 from nacl.bindings import crypto_sign_seed_keypair
 
@@ -14,50 +15,54 @@ def mnemonic_is_valid(mnemo_words: list[str]) -> bool:
     )
 
 
-def mnemonic_to_entropy(mnemo_words: list[str], password: str | None = None):
-    # TODO: implement password
+def mnemonic_to_entropy(mnemo_words: list[str]):
     sign = hmac.new(
         (" ".join(mnemo_words)).encode("utf-8"), bytes(0), hashlib.sha512
     ).digest()
     return sign
 
 
-def mnemonic_to_seed(
-    mnemo_words: list[str], seed: str, password: str | None = None
-):
-    entropy = mnemonic_to_entropy(mnemo_words, password)
+def mnemonic_to_seed(mnemo_words: list[str], seed: str):
+    entropy = mnemonic_to_entropy(mnemo_words)
     return hashlib.pbkdf2_hmac("sha512", entropy, seed, PBKDF_ITERATIONS)
 
 
-def mnemonic_to_private_key(
-    mnemo_words: list[str], password: str | None = None
-) -> tuple[bytes, bytes]:
+def mnemonic_to_private_key(mnemo_words: list[str]) -> tuple[bytes, bytes]:
     """
     :rtype: (bytes(public_key), bytes(secret_key))
     """
-    seed = mnemonic_to_seed(mnemo_words, b"TON default seed", password)
+    seed = mnemonic_to_seed(mnemo_words, b"TON default seed")
     return crypto_sign_seed_keypair(seed[:32])
 
 
-def mnemonic_to_wallet_key(
-    mnemo_words: list[str], password: str | None = None
-) -> tuple[bytes, bytes]:
+def mnemonic_to_wallet_key(mnemo_words: list[str]) -> tuple[bytes, bytes]:
     """
     :rtype: (bytes(public_key), bytes(secret_key))
     """
-    _, priv_k = mnemonic_to_private_key(mnemo_words, password)
+    _, priv_k = mnemonic_to_private_key(mnemo_words)
     return crypto_sign_seed_keypair(priv_k[:32])
 
 
-def mnemonic_new(
-    words_count: int = 24, password: str | None = None
-) -> list[str]:
+def mnemonic_new(words_count: int = 24) -> list[str]:
     while True:
         mnemo_arr = []
 
         for _ in range(words_count):
             idx = get_secure_random_number(0, len(english))
             mnemo_arr.append(english[idx])
+
+        if not is_basic_seed(mnemonic_to_entropy(mnemo_arr)):
+            continue
+
+        break
+    return mnemo_arr
+
+
+def mnemonic_from_password(password: str, words_count: int = 24) -> list[str]:
+    rnd = random.Random(password)
+
+    while True:
+        mnemo_arr = [rnd.choice(english) for _ in range(words_count)]
 
         if not is_basic_seed(mnemonic_to_entropy(mnemo_arr)):
             continue
